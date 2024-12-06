d2505d06a954c5d8481de80257b308d3a8c81118
psurinder <surinder.parsad@hotmail.com>
2024-12-07 02:35:31 +0530

    Enabled Multisite

--- wp-config.php
+++ wp-config.php
@@ -18,14 +18,14 @@
  * @package WordPress
  */
 
-//  define('WP_ALLOW_MULTISITE', true);
-//  define('MULTISITE', true);
-//  define('SUBDOMAIN_INSTALL', true);
-//  define('DOMAIN_CURRENT_SITE', 'thetriviaquestqa.com');
-//  define('PATH_CURRENT_SITE', '/');
-//  define('SITE_ID_CURRENT_SITE', 1);
-//  define('BLOG_ID_CURRENT_SITE', 1);
-//  define('COOKIE_DOMAIN', false);
+ define('WP_ALLOW_MULTISITE', true);
+ define('MULTISITE', true);
+ define('SUBDOMAIN_INSTALL', true);
+ define('DOMAIN_CURRENT_SITE', 'thetriviaquestqa.com');
+ define('PATH_CURRENT_SITE', '/');
+ define('SITE_ID_CURRENT_SITE', 1);
+ define('BLOG_ID_CURRENT_SITE', 1);
+ define('COOKIE_DOMAIN', false);
 
 
 // ** Database settings - You can get this info from your web host ** //
